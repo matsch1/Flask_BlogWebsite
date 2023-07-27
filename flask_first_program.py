f7d0c62ae59a3dd33a1204fe11f2e33c496ef41f
matsch1 <matsch1@gmx.net>
2023-07-27 11:49:52 +0200

    included bootstrap -> first changes to look like a real website

--- flask_first_program.py
+++ flask_first_program.py
@@ -1,25 +1,19 @@
 from flask import Flask, render_template
 
-# create a flask instance
 app = Flask(__name__)
 
-# create a index site (home site)
-@app.route('/')
-
+@app.route('/') # for blog posts
 def index():
-    return "<h1> Hello World! </h1>"
+    return render_template("index.html")
 
-# create some user sites
-@app.route('/user/<name>')
+@app.route('/android_apps')
+def android_apps():
+    return render_template("android_apps.html")
 
-def user(name):
-    return "<h1> Hello {}! </h1>".format(name)
+@app.route('/books')
+def books():
+    return render_template("books.html")
 
-# create a site using render_template
-@app.route('/site')
-
-def site():
-    return render_template("site1.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
