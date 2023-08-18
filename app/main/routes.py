7bfa418427ee4d4bb2ca184b622fe9159836e5fe
matsch1 <matsch1@gmx.net>
2023-08-18 20:40:56 +0200

    ready for deployment

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from app.main import bp
+from app.models.blog import Blog
 
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    post = Blog.query.get(4)
+    return render_template('index.html', post=post)
