d59787e5246ff725c21557a2ecb8700be4335e0c
Matsch <matsch1@gmx.net>
2023-08-29 16:19:02 +0200

    add possibility to change style of markdown output

--- website/models/blog.py
+++ website/models/blog.py
@@ -5,7 +5,6 @@ from markdown.extensions.toc import TocExtension
 
 from website.extensions import db
 
-
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -18,13 +17,25 @@ class Blog(db.Model):
     categories = db.Column(db.String(100), nullable=True)
     images = db.Column(db.Text, nullable=True)
 
+    
+    
     @staticmethod
     def on_changed_content(target, value, oldvalue, initiator):
-        target.content_html= markdown(value, extensions=['fenced_code', TocExtension(baselevel=3),
+        content_html= markdown(value, extensions=['fenced_code', TocExtension(baselevel=3),
 			'codehilite', 'meta', 'tables'], output_format="html5")
+        
+        target.content_html = change_html_style(content_html)
+    
+    
 
     def __repr__(self):
         return f'<Titel {self.title}>'
 
 
 db.event.listen(Blog.content, 'set', Blog.on_changed_content)
+
+def change_html_style(content_html):
+    # change link color
+    styled_content = content_html.replace('<a href', '<a class="dark_link" href')
+    
+    return styled_content
