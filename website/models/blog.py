02bc2e9ba744d1a0ef7b0da2da565a50a2a1b8b2
matsch1 <matsch1@gmx.net>
2023-08-30 11:19:11 +0200

    work on blog posts and refactoring of categorie.html

--- website/models/blog.py
+++ website/models/blog.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-import bleach
 from markdown import markdown
 from markdown.extensions.toc import TocExtension
 
 from website.extensions import db
+
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,16 +17,12 @@ class Blog(db.Model):
     categories = db.Column(db.String(100), nullable=True)
     images = db.Column(db.Text, nullable=True)
 
-    
-    
     @staticmethod
     def on_changed_content(target, value, oldvalue, initiator):
-        content_html= markdown(value, extensions=['fenced_code', TocExtension(baselevel=3),
-			'codehilite', 'meta', 'tables'], output_format="html5")
-        
+        content_html = markdown(value, extensions=['fenced_code', TocExtension(baselevel=3),
+                                                   'codehilite', 'meta', 'tables'], output_format="html5")
+
         target.content_html = change_html_style(content_html)
-    
-    
 
     def __repr__(self):
         return f'<Titel {self.title}>'
@@ -34,8 +30,10 @@ class Blog(db.Model):
 
 db.event.listen(Blog.content, 'set', Blog.on_changed_content)
 
+
 def change_html_style(content_html):
     # change link color
-    styled_content = content_html.replace('<a href', '<a class="dark_link" href')
-    
+    styled_content = content_html.replace(
+        '<a href', '<a class="dark_link" href')
+
     return styled_content
