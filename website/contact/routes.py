1ca86b6f4b58b8590144c7de3f25c4a4b59331e7
matsch1 <matsch1@gmx.net>
2023-08-30 21:14:07 +0200

    update

--- website/contact/routes.py
+++ website/contact/routes.py
@@ -2,10 +2,7 @@ from flask import render_template
 from website.contact import bp
 from website.models.blog import Blog
 
-ABOUT_ME_ID = 1
-
-
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    about_me = Blog.query.get(ABOUT_ME_ID)
+    about_me = Blog.query.get(1)
     return render_template("contact/index.html", about_me=about_me)
