00eadaf4bdd6d1f4bb098dbb9f65071a486f47bd
Matsch <matsch1@gmx.net>
2023-08-30 19:14:53 +0200

    hide database url

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config:
 
     # database
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_directory, 'app.db')
-    SQLALCHEMY_DATABASE_URI = "postgresql://zofdxfqdfwbhsf:1b936e5e3d0e5630b0b1b5174981e6952dd2862d524c22f477cf6a5bd5a61730@ec2-34-247-94-62.eu-west-1.compute.amazonaws.com:5432/dasoemou698coq"
+    SQLALCHEMY_DATABASE_URI = "DATABASE_URL"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
