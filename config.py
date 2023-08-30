5b9c424bf8213a7cc57ec1e6cdf9ec8286dcaee0
matsch1 <matsch1@gmx.net>
2023-08-30 16:14:11 +0200

    changes for using postgress db on heroku server

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
     # database
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-        os.path.join(base_directory, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_directory, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "postgres://zofdxfqdfwbhsf:1b936e5e3d0e5630b0b1b5174981e6952dd2862d524c22f477cf6a5bd5a61730@ec2-34-247-94-62.eu-west-1.compute.amazonaws.com:5432/dasoemou698coq")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
