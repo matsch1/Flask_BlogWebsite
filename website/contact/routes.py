51883cb6fe35c7b7d1fc341cd9fcbb0c23d71512
matsch1 <matsch1@gmx.net>
2023-08-30 21:12:17 +0200

    change post ID for about me in new database

--- website/contact/routes.py
+++ website/contact/routes.py
@@ -2,8 +2,10 @@ from flask import render_template
 from website.contact import bp
 from website.models.blog import Blog
 
+ABOUT_ME_ID = 1
+
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    about_me = Blog.query.get(4)
+    about_me = Blog.query.get(ABOUT_ME_ID)
     return render_template("contact/index.html", about_me=about_me)
