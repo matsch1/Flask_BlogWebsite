33e030553f5571d38170ed50835e444ab8a6f063
matsch1 <matsch1@gmx.net>
2023-09-07 11:21:10 +0200

    open links which are not in toc in a new tab

--- website/models/blog.py
+++ website/models/blog.py
@@ -35,5 +35,19 @@ def change_html_style(content_html):
     # change link color
     styled_content = content_html.replace(
         '<a href', '<a class="dark_link" href')
+    styled_content = open_links_in_new_tab(styled_content)
 
     return styled_content
+
+
+def open_links_in_new_tab(content_html):
+    split_string = '<h4 id="introduction">Introduction</h4>'
+    if split_string in content_html:
+        content_split = content_html.split(
+            split_string)
+        content_without_toc = split_string+content_split[1]
+        styled_content = content_without_toc.replace(
+            '<a class="dark_link" href', '<a class="dark_link" target="_blank" href')
+        return content_split[0] + styled_content
+    else:
+        return content_html
